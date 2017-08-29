d92242a41a5c34825f22ae7a1bdf0e9f5cb18dca
Truong Giang <truongwp@gmail.com>
2017-08-29 10:41:48 +0700

    Initial commit

---
+++ helpers.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Helper functions
+ *
+ * @package Truongwp
+ */
+
+/**
+ * Gets value from $_REQUEST.
+ *
+ * @param  string $key Data key.
+ * @return mixed
+ */
+function truongwp_request( $key ) {
+	if ( ! isset( $_REQUEST[ $key ] ) ) { // WPCS: csrf ok.
+		return;
+	}
+
+	return $_REQUEST[ $key ]; // WPCS: csrf, sanitization ok.
+}
+
+
+/**
+ * Gets value from $_POST.
+ *
+ * @param  string $key Data key.
+ * @return mixed
+ */
+function truongwp_post( $key ) {
+	if ( ! isset( $_POST[ $key ] ) ) { // WPCS: csrf ok.
+		return;
+	}
+
+	return $_POST[ $key ]; // WPCS: csrf, sanitization ok.
+}
+
+
+/**
+ * Gets value from $_GET.
+ *
+ * @param  string $key Data key.
+ * @return mixed
+ */
+function truongwp_get( $key ) {
+	if ( ! isset( $_GET[ $key ] ) ) { // WPCS: csrf ok.
+		return;
+	}
+
+	return $_GET[ $key ]; // WPCS: csrf, sanitization ok.
+}
+
+
+/**
+ * Gets current URL.
+ *
+ * @return string
+ */
+function truongwp_get_current_url() {
+	global $wp;
+	return home_url( add_query_arg( array(), $wp->request ) );
+}
+
+
+/**
+ * Pretty print_r.
+ *
+ * @param mixed $var Variable.
+ */
+function ppr( $var ) {
+	echo '<pre>';
+	print_r( $var );
+	echo '</pre>';
+}
+
+
+/**
+ * Triggers error.
+ *
+ * @param string $message Error message.
+ * @param string $type    Error type.
+ */
+function truongwp_trigger_error( $message, $type = 'notice' ) {
+	switch ( $type ) {
+		case 'error':
+			$error_type = E_USER_ERROR;
+			break;
+
+		case 'warning':
+			$error_type = E_USER_WARNING;
+			break;
+
+		case 'notice':
+			$error_type = E_USER_NOTICE;
+			break;
+
+		default:
+			$error_type = $type;
+	}
+
+	trigger_error( $message, $error_type ); // WPCS: xss ok.
+}
